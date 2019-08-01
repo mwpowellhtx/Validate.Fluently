fc468742bed3e669a649fb93925295b99c0dd82c
Michael W Powell <mwpowellhtx@gmail.com>
2019-07-31 20:58:05 -0400

    renaming the project bits in order to avoid packaging conflicts

---
+++ src/Validate.Fluently/Callbacks/FluentConditionCallback.cs
@@ -0,0 +1,10 @@
+﻿namespace Validation
+{
+    /// <summary>
+    /// Returns the <see cref="bool"/> Condition given <paramref name="obj"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public delegate bool FluentConditionCallback<in T>(T obj);
+}
